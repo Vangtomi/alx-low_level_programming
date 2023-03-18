ee1f49b19a90a0eed787aa7c0c1ba6d2998d5a22
toriolatomis@gmail.com <toriolatomis@gmail.com>
2023-03-18 09:48:03 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - UNIX is basically a simple operating system file
+ *
+ * Return: 1 (Failure)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
