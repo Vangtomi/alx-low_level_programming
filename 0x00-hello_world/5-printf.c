e5fac78ecffedd10658edb6ddad668a87e3780cc
toriolatomis@gmail.com <toriolatomis@gmail.com>
2023-03-17 22:05:33 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - This is me computing c program with proper grammer
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+		return (0);
+}
