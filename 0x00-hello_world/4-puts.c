bfd1cebf6a7fc71e306b409157f4fea99402fbc7
toriolatomis@gmail.com <toriolatomis@gmail.com>
2023-03-17 21:50:35 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - It's late at night and i am still up trying to meet second deadline
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
