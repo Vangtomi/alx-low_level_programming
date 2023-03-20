ac1ba381498bd374b8c5697d0f5bcf137db7c12f
toriolatomis@gmail.com <toriolatomis@gmail.com>
2023-03-20 03:20:21 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints lowercase alphabets in reverse
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
