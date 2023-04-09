c0029915e79bea1a3134972a51cd95ed00e7f8c1
toriolatomis@gmail.com <toriolatomis@gmail.com>
2023-04-09 10:55:14 -0700

    4addc

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -4,54 +4,47 @@
 #include <string.h>
 
 /**
- * check_num - check - strings in here are digits
- * @str: array str
+ * check_num - check if the string only contains digits
+ * @str: the string to check
  *
  * Return: 1 if the string only contains digits, 0 otherwise
  */
 int check_num(char *str)
 {
-	for (int i = 0; i < strlen(str); i++)
-	{
-		if (!isdigit(str[i]))
-	{
-		return (0);
-	}
-	}
-	return (1);
+    for (int i = 0; i < strlen(str); i++) {
+        if (!isdigit(str[i])) {
+            return 0;
+        }
+    }
+    return 1;
 }
 
 /**
  * main - a program that adds positive numbers.
- * @argc: count arguments
- * @argv: arguments
+ * @argc: the number of arguments passed to the program
+ * @argv: an array of strings containing the arguments
  *
- * Return: always 0 (success), 1 on error
+ * Return: 0 on success, 1 on error
  */
-
 int main(int argc, char **argv)
 {
-	int sum = 0;
+    int sum = 0;
 
-	if (argc < 2)
-	{
-		printf("0\n");
-		return (0);
-	}
+    if (argc < 2) {
+        printf("0\n");
+        return 0;
+    }
 
-	for (int i = 1; i < argc; i++)
-	{
-		if (check_num(argv[i]))
-		{
-			sum += atoi(argv[i]);
-		}
-		else
-		{
-			printf("Error\n");
-			return (1);
-		}
-	}
+    for (int i = 1; i < argc; i++) {
+        if (check_num(argv[i])) {
+            sum += atoi(argv[i]);
+        } else {
+            printf("Error\n");
+            return 1;
+        }
+    }
 
-	printf("%d\n", sum);
-	return (0);
+    printf("%d\n", sum);
+    return 0;
 }
+
