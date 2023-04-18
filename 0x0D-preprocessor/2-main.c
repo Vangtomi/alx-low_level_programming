0040f5b5c669319ba0a1015cb6833215250becfc
toriolatomis@gmail.com <toriolatomis@gmail.com>
2023-04-18 14:01:24 -0700

    Preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - a program that prints the name of the file it was compiled from
+ * main - prints the name of the file
  *
  * Return: Always 0 (Success)
  */
-int mai(void)
+int main(void)
 {
-	printf("%s\n",__FILE__);
+	printf("%s\n", __FILE__);
 	return (0);
 }
