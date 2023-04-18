a58bc15bb58f3323009aa07ffef08894d529f633
toriolatomis@gmail.com <toriolatomis@gmail.com>
2023-04-18 13:53:37 -0700

    Preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints the name of the file it was compiled from
+ *
+ * Return: Always 0 (Success)
+ */
+int mai(void)
+{
+	printf("%s\n",__FILE__);
+	return (0);
+}
